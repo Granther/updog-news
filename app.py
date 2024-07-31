d97aa8ff7c927f7324fe587104a26f44a5257aea
Grant Herrema <granther321@gmail.com>
2024-07-31 03:50:19 -0400

    init

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from gen_news import parse_news
 
 app = Flask(__name__)
 
@@ -7,13 +8,34 @@ links = [
     {"name": "Google", "url": "google.com"}    
 ]
 
+stories = parse_news()
+
 @app.route('/')
 def index():
-    return render_template("index.html", links=links)
+    return render_template("index.html", stories=stories)
+
+@app.route('/read_form', methods=['POST'])
+def read_form():
+    data = request.form 
+  
+    return { 
+        'title' : data['title'], 
+        'prompt': data['prompt'], 
+    } 
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route("/gen_news")
+def gen_news():
+    return render_template("gen_news.html")
+
+@app.route(f"/story/<int:idx>")
+def story(idx):
+    rend_story = stories[idx]
+    return render_template("story.html", **rend_story)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
