92392edebaef4f558e6a539882eb6dfd41dd518e
Grant Herrema <granther321@gmail.com>
2024-08-01 08:09:53 -0400

    Added story news backend, need to connect good model and make UI not suck

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-from flask import Flask, render_template, request
-from gen_news import parse_news
+from flask import Flask, render_template, request, redirect, url_for
+from gen_news import parse_news, create_story
+from dotenv import load_dotenv
+from infer import generate_news
+import os
+
+load_dotenv()
+os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
 
 app = Flask(__name__)
-
-links = [
-    {"name": "Google", "url": "google.com"},
-    {"name": "Google", "url": "google.com"}    
-]
 
 stories = parse_news()
 
 @app.route('/')
 def index():
+    stories = parse_news()
     return render_template("index.html", stories=stories)
 
 @app.route('/read_form', methods=['POST'])
 def read_form():
     data = request.form 
-  
-    return { 
-        'title' : data['title'], 
-        'prompt': data['prompt'], 
-    } 
+
+    story = generate_news(title=data['title'])
+    create_story(title=data['title'], content=story, days=data['days'])
+
+    return redirect(url_for('index'))
 
 @app.route('/about')
 def about():
@@ -31,9 +33,15 @@ def about():
 def gen_news():
     return render_template("gen_news.html")
 
-@app.route(f"/story/<int:idx>")
-def story(idx):
-    rend_story = stories[idx]
+@app.route(f"/story/<uuid>")
+def story(uuid):
+    rend_story = dict()
+
+    for story in stories:
+        if story['uuid'] == uuid:
+            print("found rend story")
+            rend_story = story
+
     return render_template("story.html", **rend_story)
 
 
--- gen_news.py
+++ gen_news.py
@@ -1,5 +1,6 @@
 from groq import Groq
-import xml.etree.ElementTree as ET 
+import xml.etree.ElementTree as ET
+import shortuuid
 import os
 
 class News:
@@ -16,13 +17,39 @@ def parse_news():
         tree = ET.parse(f"documents/{file}")
         root = tree.getroot()
 
-        doc = dict(title=root[0].text, prompt=root[1].text, length=root[2].text, content=root[3].text, idx=i)
+        doc = dict(title=root[0].text, prompt=root[1].text, length=root[2].text, content=root[3].text, days=root[4].text, uuid=root[5].text)
         news.append(doc)
+
+    news = sorted(news, key=lambda story:int(story['days']))
+    print(news)
 
     return news
 
+def create_story(title: str, content: str, prompt: str="none", length: str="none", days: str="0"):
+    root = ET.Element("document")
+    tit = ET.SubElement(root, "title") 
+    tit.text = title
 
+    prom = ET.SubElement(root, "prompt") 
+    prom.text = prompt
 
+    len = ET.SubElement(root, "length") 
+    len.text = length
 
+    cont = ET.SubElement(root, "content") 
+    cont.text = content
 
+    da = ET.SubElement(root, "days") 
+    da.text = days
 
+    ui = ET.SubElement(root, "uuid") 
+    ui.text = shortuuid.uuid()
+
+    tree = ET.ElementTree(root) 
+
+    with open(f"documents/story_{ui.text}.xml", "wb") as f:
+        tree.write(f)
+ 
+
+if __name__ == "__main__":
+    create_story(title="Hello", content="There")
