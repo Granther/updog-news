85b204adb8d4a5dbcbaf100dbf85696df7107fb5
Grant Herrema <granther321@gmail.com>
2025-01-11 12:32:21 -0500

    Removed previous unittests

--- app/infer_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from app.infer import Infer
-
-class TestInfer(unittest.TestCase):
-    def test_gen(self):
-        infer = Infer()
-        self.assertTrue(infer._gen("How are you", "You are a helpful AI", "feather"))
